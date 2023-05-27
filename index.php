4af445afd0c37717c49fb85b74da259786fe3129
RuslanAmraev <beardedsmellyfox@gmail.com>
2023-05-27 19:45:48 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./img/favicon-32x32.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./reset.css">
+    <link rel="stylesheet" href="./fonts/stylesheet.css">
+    <link rel="stylesheet" href="./fonts/bebasneue.css">
+    <link rel="stylesheet" href="./slick.css">
+    <link rel="stylesheet" href="./style.css">
+    <title>Arcane рекламное агенство</title>
+</head>
+<body>
+    <?php include('./header.html') ?>
+    <?php include('./main.html') ?>
+    <?php include('./advantages.html') ?>
+    <?php include('./switch.html') ?>
+    <script src="./jquery.js"></script>
+    <script src="./jquery.maskedinput.min.js"></script>
+    <script src="./slick.min.js"></script>
+    <script src="./script.js"></script>
+</body>
+</html>
