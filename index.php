c956fab4ccefff79784003701b155473cdff9ee5
RuslanAmraev <beardedsmellyfox@gmail.com>
2023-06-04 23:47:24 +0600

    favicon

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="icon" type="image/x-icon" href="./img/favicon-32x32.png"> -->
+    <link type="Image/x-icon" href="./img/favicon.ico" rel="icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="./img/apple-touch-icon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="./reset.css">
