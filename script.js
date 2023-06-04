79d0fca1fd6556645eef6997f4666d850730439a
RuslanAmraev <beardedsmellyfox@gmail.com>
2023-06-04 23:33:24 +0600

    1

--- script.js
+++ script.js
@@ -5,23 +5,42 @@ $(document).ready(function() {
             anchors: ['main', 'advantages', 'services', 'trial', 'footer']
         }
     );
-    $("#phone").mask("+7(999) 999-9999");
+    
+    $('phone').mask("+7(999) 999-9999")
 });
 
 
 function isVisible(el){
-    if (el.style.display === "none") {
+    if (el.style.display === "none" ) {
         return false
     }else{
         return true
     }
 }
 
-function showServicePop(id){
-    const el = document.getElementById(id)
-    if(isVisible(el)){
-        el.style.display = 'flex'
-    }else{
-        el.style.display = 'none'
+let popsFlag = false
+
+function showServicePop(e,id){
+        e.stopPropagation();
+        e.cancelBubble = true; // для IE
+        const el = document.getElementById(id)
+        if(isVisible(el)){
+            el.style.display = 'none'
+            popsFlag = false
+        }else{
+            el.style.display = 'flex'
+            popsFlag = true
+        }
+}
+
+const popsId = ['servicePopWeb', 'servicePopContext', 'servicePop']
+
+function closePops(){
+    if(popsFlag){
+        popsId.forEach(el=>{
+            document.getElementById(el).style.display = 'none'
+        })
+        popsFlag = false
+    
     }
 }
